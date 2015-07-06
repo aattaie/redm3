fe4daba02dea2cbc01e17f01aa467c21f1cf9fcd
admin0 <admin0@ubservtst.com>
2015-07-06 13:09:02 +0100

    remove ics plugin

--- plugins/question_plugin/config/routes.rb
+++
@@ -1,3 +0,0 @@
-get '/questions/', :to => "questions#index"
-get '/projects/:project_id/questions', :to => "questions#index", :as => :questions_in_project
-#get '/questions/my_issue_filter', :to => "questions#my_issue_filter"
